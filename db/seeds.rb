b8870099e53133f717c5e11adaeb538999b9298e
Carlos Villen <carlosvillen@gmail.com>
2021-06-02 14:49:56 +0100

    Added RestaurantsController and its CRUD.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ puts "Creating Restaurants"
   resto = Restaurant.create(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber,
+    phone_number: Faker::PhoneNumber.phone_number,
     category: %w[chinese italian japanese french belgian].sample
     )
 
