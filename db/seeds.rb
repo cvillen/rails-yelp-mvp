5994038b7bc3abe7b736eb9a77af31e0dcec99f1
Carlos Villen <carlosvillen@gmail.com>
2021-06-02 13:27:32 +0100

    Added reviews validations, Faker gemand added Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying records"
+
+Restaurant.destroy_all
+
+puts "Creating Restaurants"
+
+10.times do
+  resto = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber,
+    category: %w[chinese italian japanese french belgian].sample
+    )
+
+  puts "Created #{resto.name}"
+end
+
